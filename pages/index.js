25150808f4a2f4588d54c16ee6298508a8a6720a
ssuniie <phatsanphon2004@outlook.com>
2021-07-14 10:51:43 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,47 @@
+import { useEffect, useState } from 'react'
 import Head from 'next/head'
+import moment from 'moment'
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    const [todayCasesConfirmed, setTodayCasesConfirmed] = useState(0)
+    const [updateDate, setUpdateDate] = useState()
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+    useEffect(async () => {
+        let response = await fetch('https://covid19.th-stat.com/json/covid19v2/getTodayCases.json')
+        response = await response.json()
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+        let confirmed = response.NewConfirmed
+        let date = response['UpdateDate'].split(' ')
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+        setTodayCasesConfirmed(confirmed.toLocaleString())
+        setUpdateDate(`${date[0]} เวลา ${date[1]}`)
+    })
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+    return (
+        <div className="root flex flex-col items-center justify-center min-h-screen py-2">
+            <Head>
+                <title>โควิดวันนี้ติดกันกี่คน</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+            <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+                <h1 className='md:text-2xl lg:text-3xl font-light text-white m-6 drop-shadow-md'>
+                    วันนี้ติดโควิดกันกี่คน ?</h1>
+                <h3 className='md:text-8xl lg:text-9xl font-medium text-white m-6 drop-shadow-md'>
+                    {todayCasesConfirmed}</h3>
+                <p className='md:text-sm lg:text-md font-light text-white m-6 drop-shadow-md'>
+                    ข้อมูล ณ วันที่ {updateDate} (ร้าบานแม่งไม่อัพเดททุกวัน)</p>
+            </main>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <footer className="flex items-center justify-center w-full h-24 font-light text-white drop-shadow-md">
+                <div>
+                    <p>
+                        Made by Phatsanphon Nakaranurak | 
+                        API by <a href='https://www.kidkarnmai.com/'>kidkarnmai.com</a> | 
+                        Data from <a href='https://data.go.th/dataset/covid-19-daily'>data.go.th</a>
+                    </p>
+                </div>
+            </footer>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+    )
 }
